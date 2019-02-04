fd12e520260d706eec0fb87c9a1934a35800f276
Alice Liang <alicel_@alices-macbook-air.local>
2019-02-04 13:57:01 -0500

    Initial Commit

---
+++ Bip-The-Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip-The-Guy
+//
+//  Created by Alice Liang on 2/4/19.
+//  Copyright © 2019 Alice Liang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
